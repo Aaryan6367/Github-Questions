72dd12b5034fabc1bf68ebd88edf74c07ac5d79f
Aaryan Agrawal <agrawalaaryan8989@gmail.com>
2026-06-30 01:43:45 +0530

    Time: 211 ms (18.13%), Space: 109.2 MB (10.63%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
-    long long countExcellentPairs(vector<int>& vec, int k) {
-    set<int> s;
-    for (int i = 0; i < vec.size(); i++)
-    {
-        s.insert(vec[i]);
+
+long long countExcellentPairs(vector<int>& vec, int k) {
+    set<int> s(vec.begin(), vec.end());
+    
+    vector<int> countsetbits;
+    for (int num : s) {
+        countsetbits.push_back(__builtin_popcount(num));
     }
-    vector<int> nums;
-    for (auto i = s.begin(); i != s.end(); i++)
-    {
-        nums.push_back(*i);
-    }
-    vector<int> countsetbits(nums.size(),0);
-    for (int i = 0; i < nums.size(); i++)
-    {
-        countsetbits[i]=__builtin_popcount(nums[i]);
-    }
-    sort(countsetbits.begin(),countsetbits.end());
-    int i=0;
-    long long ans=0;
-    int j=countsetbits.size()-1;
-    while(i<j){
-        while(countsetbits[i]+countsetbits[j]<k && i<j){
-            i++;
-            if(i==countsetbits.size()){
-                break;
-            }
-        }
-        if(i<j){
-            long long t=j-i;
-            ans+=t;
+    
+    sort(countsetbits.begin(), countsetbits.end());
+    
+    long long ans = 0;
+    int i = 0;
+    int j = countsetbits.size() - 1;
+    
+    while (i < j) {
+        if (countsetbits[i] + countsetbits[j] >= k) {
+            ans += (j - i);
             j--;
+        } else {
+            i++;
         }
     }
-    ans*=2;
-    int p=0;
-    while(k>countsetbits[p]*2){
-        p++;
-        if(p==countsetbits.size()){
-            break;
+    
+    ans *= 2;
+    
+    for (int bits : countsetbits) {
+        if (bits * 2 >= k) {
+            ans++;
         }
     }
-    ans+=(countsetbits.size()-p);
+    
     return ans;
 }
 };
