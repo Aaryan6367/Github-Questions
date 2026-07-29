de411567114a687a34cebb9de6e08e32ac1e04ff
Aaryan Agrawal <agrawalaaryan8989@gmail.com>
2026-07-30 01:14:52 +0530

    Time: 0 ms (100%), Space: 9.9 MB (54.68%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAlphaNum(char c){
+    if(c>='0' && c<='9' || tolower(c)>='a' && tolower(c)<='z') return true;
+    else return false;
+}
+
+bool isPalindrome(string s) {
+    int n=s.size();
+    int st=0,end=n-1;
+    while(st<end){
+        if(!isAlphaNum(s[st])){
+            st++;
+            continue;
+        }
+        if(!isAlphaNum(s[end])){
+            end--;
+            continue;
+        }
+        if(tolower(s[st])!=tolower(s[end])) return false;
+        st++,end--;
+    }
+return true;
+}
+};
